a9ab54c0d32de233742328e4283bff182eaf7581
ashiksh <ashikshaheen187@gmail.com>
2021-05-29 23:16:13 +0600

    Added to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useSelector } from "react-redux";
+import Blogs from "./Components/Blogs";
+import Homepage from "./Components/Homepage";
+import Navbar from "./Components/Navbar";
+import { selectSignedIn } from "./features/userSlice";
+import "./styling/app.css";
 
-function App() {
+const App = () => {
+  const isSignedIn = useSelector(selectSignedIn);
+
   return (
-    <div>
-      
+    <div className="app">
+      <Navbar />
+      <Homepage />
+      {isSignedIn && <Blogs />}
     </div>
   );
-}
+};
 
 export default App;
+
+// OAuth Client id 837352898406-p6q5munafisueurjdv4uh2hen1o1b813.apps.googleusercontent.com
