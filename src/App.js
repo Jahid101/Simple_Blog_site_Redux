457525f8108563ca0495a1df0f91e050c876fd9e
Jahid101 <jahidhasananik.official@gmail.com>
2021-05-29 21:43:29 +0600

    Creating blogs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Blogs from './Components/Blogs';
 
 function App() {
   return (
     <div>
-      
+      <Blogs></Blogs>
     </div>
   );
 }
